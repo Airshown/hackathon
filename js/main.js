c7156ef08ccee6343a8a02d42a7ed502ee4f1394
Ludovic COURTIAL <mr.erdah@gmail.com>
2016-03-09 08:35:17 +0100

    [EDIT] Modification de l'emplacement

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function($){
 	//hide timeline blocks which are outside the viewport
 	hideBlocks(timelineBlocks, offset);
 
-/*
+
 	//on scolling, show/animate timeline blocks when enter the viewport
 	$(window).on('scroll', function(){
 		offset = 0.8;
@@ -13,7 +13,7 @@ jQuery(document).ready(function($){
 			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
 			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
 	});
-	*/
+
 
 	function hideBlocks(blocks, offset) {
 		blocks.each(function(){
@@ -26,16 +26,16 @@ jQuery(document).ready(function($){
 			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
 		});
 	}
-	
+
 	 $(".cd-timeline-block a").on("click",function() {
 		 if ($(this).parent().find('.cd-timeline-content').hasClass('is-hidden')){
        		$(this).parent().find('.cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
 		 }else{
        		$(this).parent().find('.cd-timeline-content').removeClass('bounce-in').addClass('is-hidden');
 		 }
-		 
+
     });
-	
-	
-	
+
+
+
 });
