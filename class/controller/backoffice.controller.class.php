6ae122096e7f01f36624f37982ff8953542eb35e
Philippe Pierre Granger <philgranger@orange.fr>
2016-03-10 19:36:20 +0100

    oeuhrg

--- class/controller/backoffice.controller.class.php
+++ class/controller/backoffice.controller.class.php
@@ -1,9 +1,8 @@
 <?php
 class backoffice
 {
-    public function defaultPage($args)
-    {
-		$view = new vue("index", "index", "backoffice.layout");
+    public function defaultPage($args){
+		$view = new vue("admin", "index", "backoffice.layout");
 		
 		$users = new bdd;
 		$view->assign("tableau", $users->requete("SELECT * FROM visit, user where visit.hotel = 1 and visit.user = user.id and visit.date_debut < '".date("Y-m-d H:i:s")."' and visit.date_fin > '".date("Y-m-d H:i:s")."'"));	
@@ -11,11 +10,9 @@ class backoffice
 		$view->assign("visite", $users->requete("SELECT count(*) as nombre, activities.name, visit.hotel FROM activities, feeling, visit, hotel WHERE feeling.activities = activities.id and visit.hotel = activities.hotel and hotel.id = '1' and visit.date_debut < '".date("Y-m-d H:i:s")."' and visit.date_fin > '".date("Y-m-d H:i:s")."' group by activities.id order by nombre DESC"));			
 
 		$view->assign("tauxpositif", $users->requete("SELECT AVG(REPLACE(type,'smile', '1')) as tauxpositif FROM feeling"));	
-		
-		
-		
-		
-		
-		
     }
+	
+	public function notification($args){
+		$view = new vue("admin", "notification", "backoffice.layout");
+	}
 }
