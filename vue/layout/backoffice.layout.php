0cff9ea8553256f482abb8457ed4834fab4a418e
Philippe Pierre Granger <philgranger@orange.fr>
2016-03-10 14:41:04 +0100

    oijerg

--- vue/layout/backoffice.layout.php
+++ vue/layout/backoffice.layout.php
@@ -1,3 +1,8 @@
+<?php
+print_r($tableau);
+?>
+
+
 <!DOCTYPE html>
 <!--[if IE 9 ]><html class="ie9"><![endif]-->
     <head>
@@ -317,108 +322,13 @@
                             </thead>
 					
                             <tbody>
-							<?php foreach($tableau as $key => $value): ?>
+								<?php foreach($tableau as $key => $value): ?>
                                 <tr>
                                     <td>10238</td>
                                     <td><?php echo $value["email"] ?></td>
                                     <td><?php echo $value["date_fin"]; ?></td>
                                 </tr>
 								<?php endforeach; ?>
-                                <tr>
-                                    <td>10243</td>
-                                    <td>eduardo@pingpong.com</td>
-                                    <td>19.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10248</td>
-                                    <td>eduardo@pingpong.com</td>
-                                    <td>24.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10253</td>
-                                    <td>eduardo@pingpong.com</td>
-                                    <td>29.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10234</td>
-                                    <td>lila@google.com</td>
-                                    <td>10.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10239</td>
-                                    <td>lila@google.com</td>
-                                    <td>15.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10244</td>
-                                    <td>lila@google.com</td>
-                                    <td>20.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10249</td>
-                                    <td>lila@google.com</td>
-                                    <td>25.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10237</td>
-                                    <td>robert@bingo.com</td>
-                                    <td>13.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10242</td>
-                                    <td>robert@bingo.com</td>
-                                    <td>18.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10247</td>
-                                    <td>robert@bingo.com</td>
-                                    <td>23.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10252</td>
-                                    <td>robert@bingo.com</td>
-                                    <td>28.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10236</td>
-                                    <td>simon@yahoo.com</td>
-                                    <td>12.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10241</td>
-                                    <td>simon@yahoo.com</td>
-                                    <td>17.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10246</td>
-                                    <td>simon@yahoo.com</td>
-                                    <td>22.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10251</td>
-                                    <td>simon@yahoo.com</td>
-                                    <td>27.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10235</td>
-                                    <td>tim@microsoft.com</td>
-                                    <td>11.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10240</td>
-                                    <td>tim@microsoft.com</td>
-                                    <td>16.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10245</td>
-                                    <td>tim@microsoft.com</td>
-                                    <td>21.10.2013</td>
-                                </tr>
-                                <tr>
-                                    <td>10250</td>
-                                    <td>tim@microsoft.com</td>
-                                    <td>26.10.2013</td>
-                                </tr>
                             </tbody>
                         </table>
                     </div>
