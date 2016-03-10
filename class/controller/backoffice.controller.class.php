25346c5916d13afd9015efc952ebc39d54c42621
Philippe Pierre Granger <philgranger@orange.fr>
2016-03-10 19:28:14 +0100

    mehirsug

--- class/controller/backoffice.controller.class.php
+++ class/controller/backoffice.controller.class.php
@@ -7,7 +7,7 @@ class backoffice
 		
 		$users = new bdd;
 		$view->assign("tableau", $users->requete("SELECT * FROM visit, user where visit.hotel = 1 and visit.user = user.id and visit.date_debut < '".date("Y-m-d H:i:s")."' and visit.date_fin > '".date("Y-m-d H:i:s")."'"));	
-		$view->assign("nombrevisites", $users->requete("SELECT count(*) as nombre, activities.name FROM activities, feeling WHERE feeling.activities = activities.id group by activities.id order by nombre DESC"));			
+		$view->assign("nombrevisites", $users->requete("SELECT count(*) as nombre FROM user, visit where visit.date_debut < '".date("Y-m-d H:i:s")."' and visit.date_fin > '".date("Y-m-d H:i:s")."' and visit.user = user.id"));			
 		$view->assign("visite", $users->requete("SELECT count(*) as nombre, activities.name, visit.hotel FROM activities, feeling, visit, hotel WHERE feeling.activities = activities.id and visit.hotel = activities.hotel and hotel.id = '1' and visit.date_debut < '".date("Y-m-d H:i:s")."' and visit.date_fin > '".date("Y-m-d H:i:s")."' group by activities.id order by nombre DESC"));			
 
 		$view->assign("tauxpositif", $users->requete("SELECT AVG(REPLACE(type,'smile', '1')) as tauxpositif FROM feeling"));	
