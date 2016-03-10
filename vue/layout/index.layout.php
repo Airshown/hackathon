e4ad6f79171a958893e6990aea4cc5f458efc344
Ludovic COURTIAL <mr.erdah@gmail.com>
2016-03-10 18:42:41 +0100

    [EDIT] Modification sur la page home + ajout classe active

--- vue/layout/index.layout.php
+++ vue/layout/index.layout.php
@@ -169,7 +169,7 @@
 
 						<li><a href="#" class="breakfast">Petit déjeuner</a></li>
 						<li><a href="#" class="map">Carte</a></li>
-						<li class="active"><a href="#" class="logo-active">Visit</a></li>
+						<li class="active"><a href="#" class="logo-active active">Visit</a></li>
 						<li><a href="#" class="more">Plus</a></li>
 					</ul>
 				</nav>
