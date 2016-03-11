9376ffe135f3ee221991ced922c86f8886002116
Ludovic COURTIAL <mr.erdah@gmail.com>
2016-03-11 09:00:32 +0100

    [EDIT] Corrections visuelles sur la page de login

--- vue/layout/front_login.layout.php
+++ vue/layout/front_login.layout.php
@@ -5,16 +5,16 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Visit By Best Western Admin</title>
-        
+
         <!-- Vendor CSS -->
         <link href="vue/backoffice/vendors/bower_components/animate.css/animate.min.css" rel="stylesheet">
         <link href="vue/backoffice/vendors/bower_components/material-design-iconic-font/dist/css/material-design-iconic-font.min.css" rel="stylesheet">
-            
+
         <!-- CSS -->
         <link href="vue/backoffice/css/app.min.1.css" rel="stylesheet">
         <link href="vue/backoffice/css/app.min.2.css" rel="stylesheet">
     </head>
-    
+
     <body class="login-content">
         <!-- Login -->
         <div class="lc-block toggled" id="l-login">
@@ -26,24 +26,24 @@
                     <input type="text" class="form-control" name="email" placeholder="Email">
                 </div>
             </div>
-            
+
             <div class="input-group m-b-20">
                 <span class="input-group-addon"><i class="zmdi zmdi-male"></i></span>
                 <div class="fg-line">
                     <input type="password" class="form-control" name="password" placeholder="Mot de passe">
                 </div>
             </div>
-            
+
             <div class="clearfix"></div>
-            
-            <a href="#" onclick="document.forms['form1'].submit();" class="btn btn-login btn-danger btn-float"><i class="zmdi zmdi-arrow-forward"></i></a>
+
+            <a href="#" onclick="document.forms['form1'].submit();" class="btn btn-login btn-success btn-float"><i class="zmdi zmdi-arrow-forward"></i></a>
 			</form>
-            
+
             <ul class="login-navigation">
                 <li data-block="#l-register" class="bgm-red">S'enregistrer</li>
             </ul>
         </div>
-        
+
         <!-- Register -->
         <div class="lc-block" id="l-register">
             <div class="input-group m-b-20">
@@ -51,40 +51,40 @@
                 <div class="fg-line">
                     <input type="text" name="prenom" class="form-control" placeholder="Prenom">
                 </div>
-            </div> 
-			
+            </div>
+
 			<div class="input-group m-b-20">
                 <span class="input-group-addon"><i class="zmdi zmdi-account"></i></span>
                 <div class="fg-line">
                     <input type="text" name="nom" class="form-control" placeholder="Nom">
                 </div>
             </div>
-            
+
             <div class="input-group m-b-20">
                 <span class="input-group-addon"><i class="zmdi zmdi-email"></i></span>
                 <div class="fg-line">
                     <input type="text" name="email" class="form-control" placeholder="Adresse Email">
                 </div>
             </div>
-            
+
             <div class="input-group m-b-20">
                 <span class="input-group-addon"><i class="zmdi zmdi-male"></i></span>
                 <div class="fg-line">
                     <input type="password" name="password" class="form-control" placeholder="Mot de passe">
                 </div>
             </div>
-            
+
             <div class="clearfix"></div>
-            
-           
-            
+
+
+
             <a href="" class="btn btn-login btn-danger btn-float"><i class="zmdi zmdi-arrow-forward"></i></a>
-            
+
             <ul class="login-navigation">
                 <li data-block="#l-login" class="bgm-green">Connexion</li>
             </ul>
         </div>
-        
+
         <!-- Older IE warning message -->
         <!--[if lt IE 9]>
             <div class="ie-warning">
@@ -125,21 +125,21 @@
                     </ul>
                 </div>
                 <p>Sorry for the inconvenience!</p>
-            </div>   
+            </div>
         <![endif]-->
-        
+
         <!-- Javascript Libraries -->
         <script src="vue/backoffice/vendors/bower_components/jquery/dist/jquery.min.js"></script>
         <script src="vue/backoffice/vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-        
+
         <script src="vue/backoffice/vendors/bower_components/Waves/dist/waves.min.js"></script>
-        
+
         <!-- Placeholder for IE9 -->
         <!--[if IE 9 ]>
             <script src="vendors/bower_components/jquery-placeholder/jquery.placeholder.min.js"></script>
         <![endif]-->
-        
+
         <script src="vue/backoffice/js/functions.js"></script>
-        
+
     </body>
 </html>
