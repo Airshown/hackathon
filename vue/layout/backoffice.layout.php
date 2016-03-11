469c78cbb93607ea7a58ee37e7925d32967f3938
Ludovic COURTIAL <mr.erdah@gmail.com>
2016-03-11 09:27:39 +0100

    [EDIT] correction du scalble sur le BO

--- vue/layout/backoffice.layout.php
+++ vue/layout/backoffice.layout.php
@@ -7,7 +7,27 @@
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="transparent" />
-        
+
+        <!-- Icons pack -->
+        <link rel="apple-touch-icon" sizes="57x57" href="vue/img/icons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="vue/img/icons/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="vue/img/icons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="vue/img/icons/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="vue/img/icons/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="vue/img/icons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="vue/img/icons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="vue/img/icons/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="vue/img/icons/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="vue/img/icons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="vue/img/icons/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="vue/img/icons/favicon-16x16.png">
+
+        <link rel="manifest" href="/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="vue/img/icons/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+
         <title>Visit by Best Western Admin</title>
 
         <!-- Vendor CSS -->
